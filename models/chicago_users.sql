758f406afea5075e69e5b0ce0e14ae9d007f4e84
Nicola Arnoldi <narnoldi@amazon.com>
2022-05-13 15:09:02 +0100

    adding chicago view

---
+++ models/chicago_users.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+select
+    users.userid as id,
+    users.firstname,
+    users.lastname,
+    users.city
+    
+    
+from public.users
+where users.city = 'Chicago'
